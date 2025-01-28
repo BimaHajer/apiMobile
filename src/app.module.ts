89c67f3b2431e42fe60ae8e836d5932eb90116bd
Hadhami-ghrs <hadhamigharsallah9@gmail.com>
2025-01-28 16:12:27 +0100

    userModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+    useFactory: () => ({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'foundDB',
+      autoLoadEntities: true,
+      synchronize: true,
+        
+    }),
+  }),ConfigModule.forRoot(),UserModule],
   controllers: [AppController],
   providers: [AppService],
+
 })
 export class AppModule {}
